c5391d1042af4b0005e1a4244f7389234dd9c968
Jaime <nguyenphuocdat168@gmail.com>
2024-10-07 22:38:00 -0400

    Initial Commit

---
+++ SwiftUi-Locally/SwiftUi_LocallyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_LocallyApp.swift
+//  SwiftUi-Locally
+//
+//  Created by Dat Nguyen on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_LocallyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
